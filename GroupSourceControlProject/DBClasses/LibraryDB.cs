614d488fe7d6de2bc6467f1a40862ef437bde550
Krista <37194474+KristaMarieB@users.noreply.github.com>
2018-08-07 11:02:52 -0700

    Created the Add, Updata, and Delete methods for the MemberDB and LibraryDB classes. Also added 2 properties to the Book class.

--- GroupSourceControlProject/DBClasses/LibraryDB.cs
+++ GroupSourceControlProject/DBClasses/LibraryDB.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,45 @@ namespace GroupSourceControlProject
 {
     static class LibraryDB
     {
+        public static void AddBook(Book b) //AddBookToDB?
+        {
+            // Add book to Member list VS Add Book to DB???
+            // This currently adds book to DB
+            var context = new LibraryContext();
+
+            context.Books.Add(b);
+
+            context.SaveChanges();
+        }
+
+        public static void UpdateBook(Book b) //UpdateBookStatus?
+        {
+            // User functionality VS Admin functionality???
+            var context = new LibraryContext();
+
+            Book originalBook = context.Books.Find(b.BookID);
+
+            originalBook.CheckedOut = b.CheckedOut;
+
+            context.SaveChanges();
+        }
+
+        public static void DeleteBook(Book b) //DeleteBookFromDB?
+        {
+            // Currently: Deletes Book from DB
+            // Maybe Delete Book from Member's checked out books?
+            var context = new LibraryContext();
+
+            context.Books.Add(b);
+
+            context.Entry(b).State = EntityState.Deleted;
+
+            context.SaveChanges();
+        }
+
         public static List<Book> GetAllBooks()
         {
+            // Maybe make a separate method for GetAllMembersBooks()
             throw new NotImplementedException();
         }
 
