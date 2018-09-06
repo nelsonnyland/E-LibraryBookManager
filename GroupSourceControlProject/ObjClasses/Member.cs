39d21651e8f12bfcb5cf3684b29ad238eeb89355
Nelson Nyland <nelsonnyland@gmail.com>
2018-09-06 15:55:37 -0700

    Removed Migrations and changed Book and Member to better reflect needs of database.

--- GroupSourceControlProject/ObjClasses/Member.cs
+++ GroupSourceControlProject/ObjClasses/Member.cs
@@ -1,28 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GroupSourceControlProject
 {
+    [Table("Members")]
     public class Member
     {
         [Key]
+        [StringLength(10)]
         public int MemberID { get; set; }
 
+        [Required]
         public string Username { get; set; }
 
+        [Required]
         public int PIN { get; set; }
 
+        [Required]
         public string LastName { get; set; }
 
+        [Required]
         public string FirstName { get; set; }
 
+        // false = not admin
+        // true = is admin
         public bool IsAdmin { get; set; }
-
-        public List<Book> BooksChecked { get; set; } = 
-           new List<Book>();
     }
 }
